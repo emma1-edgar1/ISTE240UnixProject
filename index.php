cbfd8ef35ca244349b96765fbf31b07120f38760
emma1-edgar1 <95726955+emma1-edgar1@users.noreply.github.com>
2023-04-18 11:53:38 -0400

    initial commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+    $title = "UNIX Survival Guide by the Pina Koalas - ISTE240";
+    $path = "./";
+    include($path . "assets/inc/header.php");
+?>
+
+
+    <div id="homepage-block">
+        <h1>Ready to Learn Unix?</h1>
+        <button>Get Started!</button>
+        <div id="homepage-h2"><h2>Know what you're looking for? Check out the topics at the top of the page.</h2></div>
+    </div>
+
+
+</div>
+</body>
+</html>
