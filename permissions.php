359c94c7897bb8a90aa5bf22d77ae923b104816e
emma1-edgar1 <95726955+emma1-edgar1@users.noreply.github.com>
2023-04-18 11:57:23 -0400

    switch to php extensions

---
+++ permissions.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Permissions - UNIX Survival Guide by the Pina Koalas - ISTE240</title>
+    <link rel="stylesheet" href="assets/css/styles.css">
+</head>
+<body>
+<div id="wrapper">
+
+</div>
+</body>
+</html>
