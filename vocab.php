a335fcfe2b2481b715fe4f3435b0a97be7f5b2c0
emma1-edgar1 <95726955+emma1-edgar1@users.noreply.github.com>
2023-04-24 23:12:45 -0400

    refactored vocab page to a grid system

--- vocab.php
+++ vocab.php
@@ -3,21 +3,18 @@
     $path = "./";
     include($path . "assets/inc/header.php");
 ?>
+    
+    <h1>Vocab Index</h1>
+    <div id="vocab-grid">
+        <h2>ls</h2>
+        <p>Short for list, this command is used to list all the files and folders in your current directory</p>
 
-        <h1>Vocab Index</h1>
-        <div class="vocab-panel">
-            <h2>ls</h2>
-            <p>Short for list, this command is used to list all the files and folders in your current directory</p>
-            <!-- <p>Can use with other commands : ls</p> -->
-        </div>
-        <div class="vocab-panel">
-            <h2 class="greyout-h2">ls</h2><h2>-la</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore sunt, molestiae veritatis quaerat</p>
-        </div>
-        <div class="vocab-panel">
-            <h2 class="greyout-h2">ls</h2><h2>-a</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam sunt veritatis, mollitia distinctio ut</p>
-        </div>
+        <div class="compound-vocab-h2"><h2 class="greyout-h2">ls</h2><h2>-la</h2></div>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore sunt, molestiae veritatis quaerat</p>
+
+        <div class="compound-vocab-h2"><h2 class="greyout-h2">ls</h2><h2>-a</h2></div>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam sunt veritatis, mollitia distinctio ut</p>
+    </div> <!--end of vocab-grid-->
 
 <?php
     include($path . "assets/inc/footer.php");
