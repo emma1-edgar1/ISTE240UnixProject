ee85d284a81fe7b37fc97692f93916a2fa7ddf2e
unknown <lac6283@rit.edu>
2023-04-30 16:16:59 -0400

    Favicon still does not work.

--- assets/inc/header.php
+++ assets/inc/header.php
@@ -4,12 +4,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" type="image/x-icon" href="../images/background.jpeg">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;0,700;1,700&display=swap" rel="stylesheet">
-    <title></title>
+    <title><?php echo $title ?></title>
+    <link rel="icon" type="image/x-icon" href="../images/favicon.png">
     <link rel="stylesheet" href="assets/css/styles.css">
+    
 </head>
 <body>
 <div id="wrapper">
