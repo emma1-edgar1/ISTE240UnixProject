376abb0a4068a27269361dc8c420ffded03115c7
unknown <lac6283@rit.edu>
2023-04-30 15:32:18 -0400

    Basic FAQ page started

--- faq.php
+++ faq.php
@@ -3,7 +3,33 @@
     $path = "./";
     include($path . "assets/inc/header.php");
 ?>
+<div>
+    <h1>Frequently Asked Questions</h1>
+    
+    <h2>What is UNIX?</h2>
+    <p>Words words words (I (Lauren) am going to add actual answers)</p>
 
+    <h2>Why is UNIX used?</h2>
+    <p>Words words words</p>
+
+    <h2>What are some important things to know about UNIX?</h2>
+    <p>Words words words</p>
+
+    <h2>How do you create, copy, and remove files?</h2>
+    <p>Words words words</p>
+
+    <h2>How do you make or remove a directory?</h2>
+    <p>Words words words</p>
+
+    <h2>What is a directory tree?</h2>
+    <p>Words words words</p>
+
+    <h2>What are the three actions you can perform on a file?</h2>
+    <p>Words words words</p>
+
+    <h2>Who has what permissions on a file?</h2>
+    <p>Words words words</p>
+</div>
 <?php
     include($path . "assets/inc/footer.php");
 ?>
