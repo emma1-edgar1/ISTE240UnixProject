89c10181f28891ff9e6c73450312e687b0231907
emma1-edgar1 <95726955+emma1-edgar1@users.noreply.github.com>
2023-04-30 15:06:26 -0400

    added next/previous link styling

--- assets/inc/footer.php
+++ assets/inc/footer.php
@@ -1,7 +1,8 @@
-<a href="previous-page" class="previous">&#8249;</a>
-<a href="next-page" class="next">&#8250;</a>
-
 </div><!--end of textBlock-->
+    <div class="next-previous-links">
+        <a href="previous-page" class="previous">&#8249; Previous page</a>
+        <a href="next-page" class="next">Next Page &#8250;</a>
+    </div>
 </div> <!--end of content-block div-->
 </div> <!--end of wrapper div-->
 </body>
