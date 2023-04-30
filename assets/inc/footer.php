e3ba71cfe03efa36a6952eaaab20135252fc694b
emma1-edgar1 <95726955+emma1-edgar1@users.noreply.github.com>
2023-04-30 14:38:00 -0400

    added previous/next buttons to the bottom of the page

--- assets/inc/footer.php
+++ assets/inc/footer.php
@@ -1,3 +1,6 @@
+<a href="previous-page" class="previous">&#8249;</a>
+<a href="next-page" class="next">&#8250;</a>
+
 </div><!--end of textBlock-->
 </div> <!--end of content-block div-->
 </div> <!--end of wrapper div-->
