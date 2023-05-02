82638d861305ce9e9534f25b8163a4de47247a9f
kjf5787 <69985740+kjf5787@users.noreply.github.com>
2023-05-01 23:47:21 -0400

    Update permissions.php

--- permissions.php
+++ permissions.php
@@ -52,7 +52,7 @@
 <h1 id="quizAnchor">Quiz Yourself!</h1>
 <h2 class="quizheaderspace">Want to test your knowledge? Take the quiz!</h2>
 
-<form method="post" class="quizForm" onsubmit="return validatePermissions();">
+<form method="post" action = "<?php echo $_SERVER['PHP_SELF']; ?>" class="quizForm" onsubmit="return validatePermissions();">
     <div class="flex-container">
         <div>
             <h4 id="permissionsq1">What are the three permission types?</h4>
