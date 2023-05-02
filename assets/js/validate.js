03019cd3c9823bda0d5b78efa8b3876418e4e8b4
kjf5787 <69985740+kjf5787@users.noreply.github.com>
2023-05-01 22:55:06 -0400

    Update validate.js

--- assets/js/validate.js
+++ assets/js/validate.js
@@ -44,3 +44,232 @@ function validateHistory(){
     	return true;
     }
 }
+
+function validateFiles(){
+	errormsg = "";
+
+	// question 1
+	if(document.getElementById("filesq1a").checked == false &&
+		document.getElementById("filesq1b").checked == false){
+		errormsg = "Please do not leave any questions blank!\nMake a guess if you need to!";
+	}
+
+	// question 2
+	if(document.getElementById("filesq2a").checked == false &&
+		document.getElementById("filesq2b").checked == false &&
+		document.getElementById("filesq2c").checked == false &&
+		document.getElementById("filesq2d").checked == false){
+		errormsg = "Please do not leave any questions blank\nMake a guess if you need to!";
+	}
+
+	// question 3
+	if(document.getElementById("filesq3a").checked == false &&
+		document.getElementById("filesq3b").checked == false){
+		errormsg = "Please do not leave any questions blank!\nMake a guess if you need to!";
+	}
+
+	// question 4
+	if(document.getElementById("filesq4a").checked == false &&
+		document.getElementById("filesq4b").checked == false){
+		errormsg = "Please do not leave any questions blank!\nMake a guess if you need to!";
+	}
+
+	// question 5
+	if(document.getElementById("filesq5a").checked == false &&
+		document.getElementById("filesq5b").checked == false){
+		errormsg = "Please do not leave any questions blank!\nMake a guess if you need to!";
+	}
+
+	// return 
+	if(errormsg != ""){
+        alert(errormsg);
+        return false;
+    }
+    else{
+    	return true;
+    }
+}
+
+function validateFolders(){
+	errormsg = "";
+
+	// question 1
+	if(document.getElementById("directq1a").checked == false &&
+		document.getElementById("directq1b").checked == false){
+		errormsg = "Please do not leave any questions blank!\nMake a guess if you need to!";
+	}
+
+	// question 2
+	if(document.getElementById("directq2a").checked == false &&
+		document.getElementById("directq2b").checked == false){
+		errormsg = "Please do not leave any questions blank\nMake a guess if you need to!";
+	}
+
+	// question 3
+	if(document.getElementById("directq3a").checked == false &&
+		document.getElementById("directq3b").checked == false){
+		errormsg = "Please do not leave any questions blank!\nMake a guess if you need to!";
+	}
+
+	// question 4
+	if(document.getElementById("directq4a").checked == false &&
+		document.getElementById("directq4b").checked == false){
+		errormsg = "Please do not leave any questions blank!\nMake a guess if you need to!";
+	}
+
+	// question 5
+	if(document.getElementById("directq5a").checked == false &&
+		document.getElementById("directq5b").checked == false){
+		errormsg = "Please do not leave any questions blank!\nMake a guess if you need to!";
+	}
+
+	// return 
+	if(errormsg != ""){
+        alert(errormsg);
+        return false;
+    }
+    else{
+    	return true;
+    }
+}
+
+function validateTerminal(){
+	errormsg = "";
+
+	// question 1
+	if(document.getElementById("terminalq1a").checked == false &&
+		document.getElementById("terminalq1b").checked == false){
+		errormsg = "Please do not leave any questions blank!\nMake a guess if you need to!";
+	}
+
+	// question 2
+	if(document.getElementById("terminalq2a").checked == false &&
+		document.getElementById("terminalq2b").checked == false &&
+		document.getElementById("terminalq2c").checked == false &&
+		document.getElementById("terminalq2d").checked == false){
+		errormsg = "Please do not leave any questions blank\nMake a guess if you need to!";
+	}
+
+	// question 3
+	if(document.getElementById("terminalq3a").checked == false &&
+		document.getElementById("terminalq3b").checked == false &&
+		document.getElementById("terminalq3c").checked == false &&
+		document.getElementById("terminalq3d").checked == false){
+		errormsg = "Please do not leave any questions blank!\nMake a guess if you need to!";
+	}
+
+	// question 4
+	if(document.getElementById("terminalq4a").checked == false &&
+		document.getElementById("terminalq4b").checked == false){
+		errormsg = "Please do not leave any questions blank\nMake a guess if you need to!";
+	}
+
+	// question 5
+	if(document.getElementById("terminalq5a").checked == false &&
+		document.getElementById("terminalq5b").checked == false){
+		errormsg = "Please do not leave any questions blank\nMake a guess if you need to!";
+	}
+
+	// return 
+	if(errormsg != ""){
+        alert(errormsg);
+        return false;
+    }
+    else{
+    	return true;
+    }
+}
+
+function validateVim(){
+	errormsg = "";
+
+	// question 1
+	if(document.getElementById("vimq1a").checked == false &&
+		document.getElementById("vimq1b").checked == false &&
+		document.getElementById("vimq1c").checked == false &&
+		document.getElementById("vimq1d").checked == false){
+		errormsg = "Please do not leave any questions blank!\nMake a guess if you need to!";
+	}
+
+	// question 2
+	if(document.getElementById("vimq2a").checked == false &&
+		document.getElementById("vimq2b").checked == false){
+		errormsg = "Please do not leave any questions blank\nMake a guess if you need to!";
+	}
+
+	// question 3
+	if(document.getElementById("vimq3a").checked == false &&
+		document.getElementById("vimq3b").checked == false &&
+		document.getElementById("vimq3c").checked == false &&
+		document.getElementById("vimq3d").checked == false){
+		errormsg = "Please do not leave any questions blank!\nMake a guess if you need to!";
+	}
+
+	// question 4
+	if(document.getElementById("vimq4a").checked == false &&
+		document.getElementById("vimq4b").checked == false){
+		errormsg = "Please do not leave any questions blank\nMake a guess if you need to!";
+	}
+
+	// question 5
+	if(document.getElementById("vimq5a").checked == false &&
+		document.getElementById("vimq5b").checked == false){
+		errormsg = "Please do not leave any questions blank\nMake a guess if you need to!";
+	}
+
+	// return 
+	if(errormsg != ""){
+        alert(errormsg);
+        return false;
+    }
+    else{
+    	return true;
+    }
+}
+
+function validatePermissions(){
+	errormsg = "";
+
+	// question 1
+	if(document.getElementById("permissionsq1a").checked == false &&
+		document.getElementById("permissionsq1b").checked == false &&
+		document.getElementById("permissionsq1c").checked == false &&
+		document.getElementById("permissionsq1d").checked == false){
+		errormsg = "Please do not leave any questions blank!\nMake a guess if you need to!";
+	}
+
+	// question 2
+	if(document.getElementById("vimpermissionsq2aq2a").checked == false &&
+		document.getElementById("permissionsq2b").checked == false){
+		errormsg = "Please do not leave any questions blank\nMake a guess if you need to!";
+	}
+
+	// question 3
+	if(document.getElementById("permissionsq3a").checked == false &&
+		document.getElementById("permissionsq3b").checked == false){
+		errormsg = "Please do not leave any questions blank!\nMake a guess if you need to!";
+	}
+
+	// question 4
+	if(document.getElementById("permissionsq4a").checked == false &&
+		document.getElementById("permissionsq4b").checked == false &&
+		document.getElementById("permissionsq4c").checked == false &&
+		document.getElementById("permissionsq4d").checked == false){
+		errormsg = "Please do not leave any questions blank\nMake a guess if you need to!";
+	}
+
+	// question 5
+	if(document.getElementById("permissionsq5a").checked == false &&
+		document.getElementById("permissionsq5b").checked == false){ 
+		errormsg = "Please do not leave any questions blank\nMake a guess if you need to!";
+	}
+
+	// return 
+	if(errormsg != ""){
+        alert(errormsg);
+        return false;
+    }
+    else{
+    	return true;
+    }
+}
