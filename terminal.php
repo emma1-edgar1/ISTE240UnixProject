5ca136a513896124163d3806b372245b1dcd74c6
kjf5787 <69985740+kjf5787@users.noreply.github.com>
2023-05-01 23:46:52 -0400

    Update terminal.php

--- terminal.php
+++ terminal.php
@@ -40,7 +40,7 @@
 <h1 id="quizAnchor">Quiz Yourself!</h1>
 <h2 class="quizheaderspace">Want to test your knowledge? Take the quiz!</h2>
 
-<form method="post" class="quizForm" onsubmit="return validateTerminal();">
+<form method="post" action = "<?php echo $_SERVER['PHP_SELF']; ?>" class="quizForm" onsubmit="return validateTerminal();">
     <div class="flex-container">
         <div>
             <h4 id="terminalq1">The Root folder does not contain important folders: </h4>
